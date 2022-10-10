5671e79a042dc60bf0d6654f69925655dfae4aed
Jeppe Qin Hansen <jeppeqin@gmail.com>
2022-10-10 19:04:25 +0200

    guides

--- screens/GuideScreen.js
+++ screens/GuideScreen.js
@@ -1,21 +1,7 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import TipsTricksBeginner from '../components/guides/TipsTricksBeginner';
-import { NavigationContext } from '../contexts/NavigationContext';
 
 const GuideScreen = ({ route, navigation }) => {
-  const nav = useContext(NavigationContext)
-
-  useEffect(() => {
-    nav.setBack(true)
-  }, [])
-
-  useEffect(() => {
-    if (nav.goBack) {
-      nav.reset()
-      navigation.navigate('Home')
-    }
-  }, [nav.goBack])
-
   if (route.params.guide === 1) {
     return <TipsTricksBeginner />
   }
